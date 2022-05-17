dd8b4f0f8f141eb6ddbb31fb32d5ecd85d911cbc
Tomasz <lis109@wp.pl>
2022-05-17 19:37:07 +0200

    feature new class WebSecurityConfiguration

---
+++ src/main/java/com/project/travelAgency/web/WebSecurityConfiguration.java
@@ -0,0 +1,10 @@
+package com.project.travelAgency.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+}
